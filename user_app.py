3b694fa79dc3aee0a2ed59e83b0f191007ea8e27
francov240 <franviotti@gmail.com>
2023-10-31 00:31:18 -0300

    Script con mas forma

--- user_app.py
+++ user_app.py
@@ -1,73 +1,111 @@
+"""
+    Script para comunicacion serial con FPGA
+
+    Parameters:
+    port (str): Puerto a utilizar
+
+    Returns:
+    Resultado de la operación entre dos operandos
+"""
+import sys
 import serial
+
+if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+    raise ValueError("El número de puerto serial es inválido.")
+
+port_number = sys.argv[1]
+PORT = f'/dev/ttyUSB{int(port_number)}'
 
 # Configurar el puerto serial
 # Si se le pasa el puerto (port!=None), se abre el puerto serial
 serial_port = serial.Serial(
-                                port="/dev/ttyUSB0",
-                                baudrate=19200,
-                                bytesize=8,
-                                parity="PARITY_NONE",
-                                stopbits="STOPBITS_ONE")  # Reemplazar '/dev/ttyUSB0' con el puerto serial apropiado
+    port=PORT,
+    baudrate=19200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=0
+)
 
-# Función para enviar un dato a la FPGA
 def send_data(data):
-    serial_port.write(data)
+    """
+    Función para enviar un dato a la FPGA
 
-# Función para recibir datos de la FPGA
+    Parameters:
+    data (bytes): Dato a enviar a la FPGA
+
+    Returns:
+    None
+    """
+    serial_port.write(int(data).to_bytes(1, 'big'))
+
 def read_data(size):
+    """
+    Función para recibir datos de la FPGA
+
+    Parameters:
+    size (int): Cantidad de bytes a leer de la FPGA
+
+    Returns:
+    bytes: Datos recibidos de la FPGA
+    """
     return serial_port.read_until(size)
 
-# Función que transforma en binario el código de operación
-def code_to_bin(op_code):
-    return {
-        'ADD': "100000",
-        'SUB': "100010",
-        'AND': "100100",
-        'OR': "100101",
-        'XOR': "100110",
-        'SRA': "000011",
-        'SRL': "000010",
-        'NOR': "100111"
-    }.get(op_code, None)
+def code_to_bin(code):
+    """
+    Función que transforma en binario el código de operación
 
-# Función que transforma en binario un operando
-def int_to_bin8(operand):
-    if operand < -128 or operand > 127:
-        raise ValueError("El operando debe estar en el rango de -128 a 127 para ser representado con 8 bits signados.")
-    if operand >= 0:
-        return bin(operand)[2:].zfill(8)
-    else:
-        return bin(256 + operand)[2:]
+    Parameters:
+    code (str): Código de operación a transformar en binario
+
+    Returns:
+    str: Código de operación en binario
+    """
+    return {
+        'ADD': 0b100000,
+        'SUB': 0b100010,
+        'AND': 0b100100,
+        'OR' : 0b100101,
+        'XOR': 0b100110,
+        'SRA': 0b000011,
+        'SRL': 0b000010,
+        'NOR': 0b100111
+    }.get(code, None)
+
+# Se limpian las pilas de datos no leídos o escritos pero no enviados
+serial_port.flushInput()
+serial_port.flushOutput()
 
 try:
     # Pedir datos al usuario
     valid_ops = ["ADD", "SUB", "AND", "OR", "XOR", "SRA", "SRL", "NOR"]
-    op_code = input("Ingrese el código de operación: ")
+    op_code = input("Ingrese uno de los siguientes códigos de operación: \
+                    ADD, SUB, AND, OR, XOR, SRA, SRL, NOR ")
 
     # Verificar si el código de operación es válido y transformar en binario
     if op_code not in valid_ops:
-        raise ValueError("Operación inválida. Las operaciones válidas son ADD, SUB, AND, OR, XOR, SRA, SRL, NOR. Intente de nuevo.")
+        raise ValueError("Operación inválida. Intente de nuevo.")
     op_code = code_to_bin(op_code)
 
     # Solicitar y verificar los operandos
     data_a = int(input("Ingrese el primer operando: "))
     if data_a < -128 or data_a > 127:
-        raise ValueError("El primer operando debe ser un número entero representable con 8 bits signados.")
-    data_a = int_to_bin8(data_a)
+        raise ValueError("El primer operando debe ser un número\
+                          entero representable con 8 bits signados.")
 
     data_b = int(input("Ingrese el segundo operando: "))
     if data_b < -128 or data_b > 127:
-        raise ValueError("El segundo operando debe ser un número entero representable con 8 bits signados.")
-    data_b = int_to_bin8(data_b)
+        raise ValueError("El segundo operando debe ser un número\
+                          entero representable con 8 bits signados.")
 
     # Enviar cada dato a la FPGA
     serial_port.send_data(op_code)
     serial_port.send_data(data_a)
     serial_port.send_data(data_b)
 
-    # Leer la respuesta de la FPGA
-    result = serial_port.read_data(8)  # Lee 8 bits de datos de la FPGA
-    print('Datos recibidos de la FPGA:', result)
+    # Leer el resultado de la FPGA
+    result = serial_port.read_data(1)
+    print(f"The result of {data_a} {op_code} {data_b} is: {result}")
 
 except ValueError as ve:
     print(ve)
